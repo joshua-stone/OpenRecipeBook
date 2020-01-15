eecea6d16f7fd4c74a3077cf76ad5e1904be47e7
Minty Fresh <mintiewute@gmail.com>
2020-01-15 01:13:56 -0500

    Add scaffold generator for ingredients.

---
+++ src/bin/generate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from scaffold_generators.ingredient import run_ingredient_scaffold_generator
+from sys import argv, exit
+
+TYPE_MAPPINGS = {
+    'ingredient': run_ingredient_scaffold_generator
+}
+
+def run_scaffold_generator(type, file_name):
+    if type not in TYPE_MAPPINGS:
+        raise ValueError(f"Unsupported type: `{type}`.\nSupported types are: {', '.join(TYPE_MAPPINGS.keys())}.")
+
+    TYPE_MAPPINGS.get(type)(file_name)
+
+if __name__ == '__main__':
+    try:
+        if len(argv) != 3:
+            raise ValueError('Syntax: generate.py [TYPE] [output-file-name]')
+
+        type, file_name = argv[1:]
+        run_scaffold_generator(type, file_name)
+
+    except Exception as e:
+        print(e)
+        exit(1)
