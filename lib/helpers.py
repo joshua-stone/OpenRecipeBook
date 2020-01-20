14c0edafeb7bb8a786c9655fd1c63ac1886db392
Joshua Stone <joshua.gage.stone@gmail.com>
2020-01-20 12:34:35 -0500

    Add initial edition description file

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,4 +1,11 @@
-def join_params(old_params, new_params):
-  new_dict = dict(list(old_params.items()) + list(new_params.items()))
+from os import walk
+from os.path import join, splitext
+from glob import glob
 
-  return new_dict
+def join_params(old_params, new_params):
+    new_dict = dict(list(old_params.items()) + list(new_params.items()))
+
+    return new_dict
+
+def list_files(path, pattern:
+    return [result for value in walk(path) for result in glob(join(value[0], pattern))]
